1e807229a9036d8ce9bcefaa2c12997c007c98a9
Thomas A <thomas.aftandilian@gmail.com>
2026-09-05 15:27:32 -0700

    added starter code

---
+++ 282A/Ethan.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Scanner allows the program to read user input
+        Scanner scanner = new Scanner(System.in);
+    }
+}
